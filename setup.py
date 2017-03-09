6d5063152882e4b825ce255c4764748207c63b0b
Matt Graham <m.m.graham@ed.ac.uk>
2017-03-09 21:51:57 +0000

    Changing to setuptools setup script.

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python2
 
-from distutils.core import setup
+from setuptools import setup
 
 if __name__ == '__main__':
-    setup(name='bvh',
-          description='Biviosion Hierarchy data loading and rendering',
-          author='Matt Graham',
-          packages=['bvh'])
+    setup(
+        name='bvh',
+        description='Biviosion Hierarchy data loading and rendering',
+        author='Matt Graham',
+        url='https://github.com/matt-graham/bvh-tools',
+        packages=['bvh']
+    )
